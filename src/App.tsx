413ab2cbbe0fc4f48d17c72dae814b23935fd720
darktend <softwaredarktend@gmail.com>
2021-07-21 01:20:52 +0300

    bad commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.scss';
-import React, { Component }  from 'react';
+import logo from "./logo.svg";
+import "./App.scss";
+import React, { Component } from "react";
 
 function App() {
   return (
@@ -8,9 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.  </p>
-    <a
-          className="App-link"
+          Edit <code>src/App.js</code> and save to reload.{" "}
+        </p>
+        <a      className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
