0412f36d064ec3e7438e021a77d7f5cbda32d1d9
darktend <softwaredarktend@gmail.com>
2021-07-22 21:22:42 +0300

    DEV-12: add prettier & husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     'es2021': true,
     'node': true,
   },
-  'extends': ["plugin:react/recommended", "google", "prettier"],
+  'extends': ["plugin:react/recommended", "prettier"],
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
     'ecmaFeatures': {
