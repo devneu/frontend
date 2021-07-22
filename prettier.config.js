23555c4eb154bb4974a74cc74c3345a5e5e1561c
darktend <softwaredarktend@gmail.com>
2021-07-22 20:39:26 +0300

    bad commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports={
+  printWidth:100,
+  singleQuote: true,
+  traillingComma: "all",
+  bracketSpacing:true,
+  jsxBracketSameLine:false,
+  tabWidth:2,
+  semi:true,
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+let person = {
+  name: "Yoda",
+};
+
+function erwer(parasdsdms) {
+  sdsadasd.parasdsdms = 12;
+}
