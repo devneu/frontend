c7edb8eb2fa16d4d912718ef1d8d7020ebf055fa
Vlad Dr.Frame <remenjuk2010@gmail.com>
2021-07-23 10:33:40 +0300

    DEV-5: added ant lib
    Added ant-lib + nav component

--- src/App.tsx
+++ src/App.tsx
@@ -30,9 +30,6 @@ function App() {
     </Switch>
       </main>
       </Suspense>
-      
-      
-    
       </>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,21 +1,57 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./Navigation.scss";
+import "antd/dist/antd.css";
 import { NavLink } from "react-router-dom";
+import { useLocation } from "react-router";
+import { Layout, Menu } from "antd";
+import { DeploymentUnitOutlined } from "@ant-design/icons";
+
+const { Header } = Layout;
 
 export default function Navigation() {
+  const [currentPage, setCurrentPage] = useState("bla");
+  const location = useLocation();
+
+  const handleCurrentPageNumber = useCallback(() => {
+    switch (location.pathname) {
+      case "/":
+        setCurrentPage("1");
+        break;
+      case "/weather":
+        setCurrentPage("2");
+        break;
+      case "/chat":
+        setCurrentPage("3");
+        break;
+      default:
+        setCurrentPage("1");
+        break;
+    }
+  });
+
+  useEffect(() => {
+    handleCurrentPageNumber();
+  }, [handleCurrentPageNumber, currentPage]);
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/weather">Weather</NavLink>
-        </li>
-        <li>
-          <NavLink to="/chat">Chat</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+      <nav className="Navigation">
+        <DeploymentUnitOutlined
+          className="Navigation__logo"
+          style={{ fontSize: "30px", color: "#ffffff" }}
+        />
+        <Menu theme="dark" mode="horizontal" selectedKeys={currentPage}>
+          <Menu.Item key="1">
+            <NavLink to="/">Home</NavLink>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <NavLink to="/weather">Weather</NavLink>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <NavLink to="/chat">Chat</NavLink>
+          </Menu.Item>
+        </Menu>
+      </nav>
+    </Header>
   );
 }
