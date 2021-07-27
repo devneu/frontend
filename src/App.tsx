381511972907265405ab31cabeecc75af92677b1
SERGIO-LAPTOP\Sarma <sarmat.p8@gmail.com>
2021-07-27 21:43:59 +0200

    DEV-30-Create-404-page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import ReduxExampleContainer from './components/ReduxExample'
 
 import Navigation from './components/Navigation'
 import Fallback from './components/Fallback/Fallback'
+import Page404 from './components/Page404/Page404'
 
 const Home = lazy(() => import('./pages/Home/Home' /*webpackChunkName: "HomePage" */))
 const Chat = lazy(() => import('./pages/Chat/Chat' /*webpackChunkName: "ChatPage" */))
@@ -27,11 +28,14 @@ function App(): ReactElement {
             <Route path='/weather'>
               <Weather />
             </Route>
-          </Switch>
-
+            <Route path='*'>
+              <Page404 />
+            </Route>
+          </Switch> 
           <ReduxExampleContainer />
         </main>
       </Suspense>
+
     </>
   );
 }
