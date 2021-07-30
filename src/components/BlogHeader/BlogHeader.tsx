265ba3f152db25f29ce43f3556bbfda059e9ac2b
Oleh <olehrudak981@gmail.com>
2021-07-30 14:40:01 +0300

    DEV-32. BlogHeader Component added(ant design). Blog page added. (#11)
    * DEV-32. BlogHeader Component added(ant design). Blog page added.
    
    * DEV-32. Requested changes resolved

---
+++ src/components/BlogHeader/BlogHeader.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Typography, PageHeader } from 'antd';
+import './BlogHeader.scss'
+
+const { Title } = Typography;
+
+interface BlogHeaderProps {
+   title: string,
+   subtitle: string
+}
+const BlogHeader = ({ title, subtitle }: BlogHeaderProps) => {
+   return (
+      <PageHeader
+         className="blog-header">
+         <Title
+            level={1}
+            className="blog-header__title">{title}</Title>
+         <Title
+            level={3}
+            type="secondary"
+            className="blog-header__subtitle">{subtitle}</Title>
+      </PageHeader>
+
+
+   )
+}
+
+export default BlogHeader;
