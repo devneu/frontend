a7b7bc53d5a1a8ec4cd54d694b3f11f2fc7dda1b
darktend <softwaredarktend@gmail.com>
2021-07-30 23:31:27 +0300

    DEV-12: Add-prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import logo from "./logo.svg";
-import "./App.scss";
-import React from "react";
+import logo from './logo.svg';
+import './App.scss';
+import React from 'react';
 
 function App() {
   return (
