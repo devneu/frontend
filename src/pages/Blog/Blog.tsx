d32309bf78579d91c51c41104e75ca563842f724
olehrdk <olehrudak981@gmail.com>
2021-08-02 14:53:14 +0300

    DEV-39. Pagination added to Blog page

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import Layout, { Content } from 'antd/lib/layout/layout';
 import BlogHeader from '../../components/BlogHeader';
 import BlogPagination from '../../components/BlogPagination';
 import './Blog.scss';
