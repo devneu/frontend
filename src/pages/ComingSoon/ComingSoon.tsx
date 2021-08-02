1fc3b2fb14cfd8877d27dc3617ef08fe96f5563e
OlegDsalo <87490184+olegdsalo@users.noreply.github.com>
2021-08-02 22:59:57 +0300

    add video

--- src/pages/ComingSoon/ComingSoon.tsx
+++ src/pages/ComingSoon/ComingSoon.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "./ComingSoon.scss";
 import {Input, Button, Typography} from 'antd';
-// import  bgVideo from './video/Gluehlampen1_6.mp4'
-
+import ReactPlayer from "react-player";
 const {Title} = Typography;
-
 
 
 const ComingSoon = () => {
     return (
-        <div className="ComingSoon">
-            <header className="coming-soon-header">
-                    <div className="left-block">
+        <div>
+            <ReactPlayer className="bg-video" url='https://www.youtube.com/watch?v=sRE5iQCdRvE' loop muted
+                         playing={true}/>
+            <div className="coming-soon-page">
+                <header className="coming-soon-header">
+                    <div className="form-block-coming-soon">
                         <Title level={1}>Our Website is Coming Soon</Title>
                         <p>We're working hard to finish the development of the site.Sign up
                             below to receive updates and to be noticed when we launch!</p>
@@ -20,17 +21,18 @@ const ComingSoon = () => {
                             <Button type="primary">NOTIFY ME!</Button>
                         </div>
 
-                </div>
-                <div className="right-block">
-                    <div className="social"><a href="#"><img
-                        src="https://image.flaticon.com/icons/png/128/1384/1384017.png"
-                        alt="twiter"/></a></div>
-                    <div className="social"><a href="#"><img
-                        src="https://image.flaticon.com/icons/png/128/2111/2111708.png"
-                        alt="tel"/></a></div>
-                </div>
-            </header>
-        </div>
+                    </div>
+                    <div className="social-links">
+                        <div className="social"><a href="#"><img
+                            src="https://image.flaticon.com/icons/png/128/1384/1384017.png"
+                            alt="twiter"/></a></div>
+                        <div className="social"><a href="#"><img
+                            src="https://image.flaticon.com/icons/png/128/2111/2111708.png"
+                            alt="tel"/></a></div>
+                    </div>
+                </header>
+            </div>
+          </div>
     );
 }
 
