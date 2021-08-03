d2a736064a8d828a1c0560842912bd681ba62ce0
OlegDsalo <87490184+olegdsalo@users.noreply.github.com>
2021-08-03 21:13:22 +0300

    add folder images and use images from folder ,fix className

--- src/pages/ComingSoon/ComingSoon.tsx
+++ src/pages/ComingSoon/ComingSoon.tsx
@@ -1,37 +1,40 @@
-import React from "react";
-import "./ComingSoon.scss";
+import React from 'react';
+import './ComingSoon.scss';
 import {Input, Button, Typography} from 'antd';
-import ReactPlayer from "react-player";
+import ReactPlayer from 'react-player';
+import telegram from './images/telegram.png';
+import twitter from './images/twitter.png';
+
+
+
+
 const {Title} = Typography;
-
-
 const ComingSoon = () => {
     return (
-        <div>
+        <div className="coming-soon-page" >
             <ReactPlayer className="bg-video" url='https://www.youtube.com/watch?v=sRE5iQCdRvE' loop muted
                          playing={true}/>
-            <div className="coming-soon-page">
                 <header className="coming-soon-header">
-                    <div className="form-block-coming-soon">
+                    <div className="subscribe-form">
                         <Title level={1}>Our Website is Coming Soon</Title>
                         <p>We're working hard to finish the development of the site.Sign up
                             below to receive updates and to be noticed when we launch!</p>
-                        <div className="form">
+                        <div className="row-input-mail">
                             <Input type="large" placeholder="Enter Email address..."></Input>
                             <Button type="primary">NOTIFY ME!</Button>
                         </div>
                     </div>
                     <div className="social-links">
-                        <div className="social"><a href="#"><img
-                            src="https://image.flaticon.com/icons/png/128/1384/1384017.png"
-                            alt="twiter"/></a></div>
-                        <div className="social"><a href="#"><img
-                            src="https://image.flaticon.com/icons/png/128/2111/2111708.png"
-                            alt="tel"/></a></div>
+                        <div className="social-link"><a href="#"><img
+                            src={twitter}
+                            alt="twitter"/></a></div>
+
+                        <div className="social-link"><a href="#"><img
+                            src={telegram}
+                            alt="telgram"/></a></div>
                     </div>
                 </header>
-            </div>
-          </div>
+        </div>
     );
 }
 
