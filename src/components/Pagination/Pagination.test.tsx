eb5dafb2a3c25da42b4a114e9b0ad37c58027f0a
olehrdk <olehrudak981@gmail.com>
2021-08-11 18:24:19 +0300

    DEV-39. Error in Pagination.test removed

--- src/components/Pagination/Pagination.test.tsx
+++ src/components/Pagination/Pagination.test.tsx
@@ -4,7 +4,16 @@ import Pagination from './Pagination';
 
 describe('Pagination', () => {
   it('Should successfully render', () => {
-    const wrapper = renderer.create(<Pagination totalItems={500} pageSize={10} />).toJSON();
+    const wrapper = renderer
+      .create(
+        <Pagination
+          totalItems={500}
+          pageSize={10}
+          currentPage={1}
+          onPageChange={() => console.log('change page')}
+        />,
+      )
+      .toJSON();
     expect(wrapper).toMatchSnapshot();
   });
 });
