5135394d59486c9447aad5ee18708a0170170dab
Oleh <olehrudak981@gmail.com>
2021-08-11 18:34:33 +0300

    Dev 39 create pagination (#22)
    * DEV-39. Pagination added
    
    * DEV-39. Pagination added to Blog page
    
    * DEV-39. Merge conflicts resolved
    
    * DEV-39. Eslint errors resolved
    
    * DEV-39. Pagination added to storybook
    
    * DEV-39. onPageChange moved to Blog.tsx
    
    * DEV-39. Error in Pagination.test removed

---
+++ src/stories/Pagination.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Pagination from '../components/Pagination/Pagination';
+
+export default {
+  title: 'Pagination',
+  component: Pagination,
+  argTypes: { backgroundColor: { control: 'color' } },
+} as ComponentMeta<typeof Pagination>;
+
+const Template: ComponentStory<typeof Pagination> = (args) => <Pagination {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
