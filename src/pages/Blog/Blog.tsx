d16281f522faf7a8518b39fa5013c7c9c52db998
Skitus <80597741+Skitus@users.noreply.github.com>
2021-08-12 15:07:47 +0300

    DEV-35: create small card post V2 and add test (#27)
    * DEV-35: create small card post V2 and add test
    
    * Dev-35: PR changes and add tests to components
    
    * DEV-35: fix PR changes no review
    
    * DEV-35: create feature post
    
    * DEV-35: merge conflict with pagination
    
    * DEv-35: fix PR changes and add components to storybook, also add tests

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import BlogHeader from '../../components/BlogHeader';
 import Pagination from '../../components/Pagination';
 import './Blog.scss';
-import { Search } from '../../components/BlogSearch/Search';
-
 
 function Blog() {
   const [page, onPageChange] = useState(1);
@@ -13,11 +11,9 @@ function Blog() {
         title="Welcome to our blog"
         subtitle="This is subtitle. Lorem ipsum dolor sit amet."
       />
-       <Search />
       <Pagination totalItems={51} pageSize={10} currentPage={page} onPageChange={onPageChange} />
     </>
   );
 }
-
 
 export default Blog;
