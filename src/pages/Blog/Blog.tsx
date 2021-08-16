74e0458e7e7c2e3b4927b3013ba1af6f5fcb0dc5
Skitus <80597741+Skitus@users.noreply.github.com>
2021-08-16 17:03:26 +0300

    DEV-36: create search (#29)
    * DEV-36: create search
    
    * DEV-36: fix merge conficts
    
    * DEV-35: create small card post V2 and add test (#27)
    
    * DEV-35: create small card post V2 and add test
    
    * Dev-35: PR changes and add tests to components
    
    * DEV-35: fix PR changes no review
    
    * DEV-35: create feature post
    
    * DEV-35: merge conflict with pagination
    
    * DEv-35: fix PR changes and add components to storybook, also add tests
    
    * DEV-36: fix layout from
    
    * DEV-26: fix search widget
    
    * DEV-36: add tests and storybook
    
    * DEV-36: PR changes move my components to another folder and add new component
    
    * DEV-36: fix PR changes
    
    * DEV-36: delete max width component
    
    * DEV-36: fix styles PR changes
    
    * DEV-36: change row and approved

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -1,9 +1,11 @@
+import { Col, Row } from 'antd';
 import React, { useState } from 'react';
 import BlogHeader from '../../components/BlogHeader';
 import Pagination from '../../components/Pagination';
 import './Blog.scss';
 import PostsCardList from '../../components/BlogPosts/PostCardList';
 import { mockPosts } from '../../components/BlogPosts/data';
+import SearchWidget from '../../components/Search';
 
 const Blog = () => {
   const [featurePost, ...posts] = mockPosts;
@@ -11,8 +13,20 @@ const Blog = () => {
   return (
     <>
       <BlogHeader title="Post Title" subtitle="Post Subtitle" />
-      <PostsCardList posts={posts} featurePost={featurePost} />
-      <Pagination totalItems={51} pageSize={10} currentPage={page} onPageChange={onPageChange} />
+      <Row className="container" justify="center">
+        <Col span={14}>
+          <PostsCardList posts={posts} featurePost={featurePost} />
+          <Pagination
+            totalItems={51}
+            pageSize={10}
+            currentPage={page}
+            onPageChange={onPageChange}
+          />
+        </Col>
+        <Col span={6} className="sidebar-blog-post">
+          <SearchWidget />
+        </Col>
+      </Row>
     </>
   );
 };
