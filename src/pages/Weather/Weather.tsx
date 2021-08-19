566139ad57747b1c844565034cad9a42cf7b39b4
OlegDsalo <87490184+OlegDsalo@users.noreply.github.com>
2021-08-19 19:05:31 +0300

    add icon

--- src/pages/Weather/Weather.tsx
+++ src/pages/Weather/Weather.tsx
@@ -3,6 +3,7 @@ import './Weather.scss';
 import axios from 'axios';
 import Loader from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import { Input } from 'antd';
 
 const Weather = () => {
   const [loading, setLoaded] = useState(true);
@@ -10,7 +11,7 @@ const Weather = () => {
   useEffect(() => {
     axios
       .get(
-        'http://api.openweathermap.org/data/2.5/weather?units=metric&id=702550&appid=fc6916988b0954c2540d0f09fb7e8a65',
+        'http://api.openweathermap.org/data/2.5/weather?units=metric&q=London&appid=fc6916988b0954c2540d0f09fb7e8a65',
       )
       .then((response) => {
         // handle success
@@ -27,9 +28,14 @@ const Weather = () => {
     <div className="weather-components">
       <Loader visible={loading} type="Circles" color="#00BFFF" height={200} width={200} />
       <div className="weather-block">
+        <Input placeholder="enter city" />
         <div className="weather-city">{wdata?.name}</div>
         <div className="weather-description">{wdata?.weather?.[0]?.description}</div>
-        <div className="weather-icon">{wdata?.weather?.[0]?.icon}</div>
+        <img
+          className="weather-icon"
+          src={`https://openweathermap.org/img/wn/${wdata?.weather?.[0]?.icon}@2x.png`}
+          alt="icon"
+        />
         <div className="weather-temp">
           {wdata?.main?.temp}
           &deg;
@@ -40,3 +46,5 @@ const Weather = () => {
 };
 export default Weather;
 // <div>{JSON.stringify(wdata, null, 2)}</div>
+// <img className="weather-icon" src={`https://openweathermap.org/img/wn/${wdata?.weather?.[0]?.icon}@2x.png`}/>
+// <div className="weather-icon">{wdata?.weather?.[0]?.icon}</div>
