2af68339c856c1063ff9376199ac8979a9feddbf
Skitus <artur.demenskiy03@gmail.com>
2021-08-23 16:26:45 +0300

    DEV-30: fix PR changes

--- src/components/PageNotFound/PageNotFound.tsx
+++ src/components/PageNotFound/PageNotFound.tsx
@@ -1,36 +1,26 @@
-import React from "react";
-import { Empty, Typography, Anchor } from "antd";
-import "antd/dist/antd.css";
-import "./PageNotFound.scss";
-import { ArrowLeftOutlined } from "@ant-design/icons";
+import React from 'react';
+import { Empty, Typography, Anchor } from 'antd';
+import 'antd/dist/antd.css';
+import './PageNotFound.scss';
+import { ArrowLeftOutlined } from '@ant-design/icons';
 
 const { Title, Paragraph, Text } = Typography;
 const { Link } = Anchor;
 
-const PageNotFound = () => {
-  return (
-    <Typography>
-      <Empty
-        image={Empty.PRESENTED_IMAGE_SIMPLE}
-        description={false}
-        className="page-not-found"
-      >
-        <Title level={1}>404</Title>
-        <Text type="secondary">Page Not Found</Text>
-        <br />
-        <br />
-        <Paragraph>
-          It looks like you found a glitch in the matrix...
-        </Paragraph>
-        <div className="link-to-home-page">
-        <ArrowLeftOutlined style={{ color: "#1890FF"}}/>
-        <Anchor>
-          <Link href="/" title="Back to Home Page" />
-        </Anchor>
-          </div>
-      </Empty>
-    </Typography>
-  );
-};
+const PageNotFound = () => (
+  <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={false} className="page-not-found">
+    <Title level={1}>404</Title>
+    <Text type="secondary">Page Not Found</Text>
+    <br />
+    <br />
+    <Paragraph>It looks like you found a glitch in the matrix...</Paragraph>
+    <div className="link-to-home-page">
+      <ArrowLeftOutlined />
+      <Anchor>
+        <Link href="/" title="Back to Home Page" />
+      </Anchor>
+    </div>
+  </Empty>
+);
 
 export default PageNotFound;
