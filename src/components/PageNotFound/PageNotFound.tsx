70c097cb4b1962ee5c675b2ed6c2c9da92b435c6
Skitus <artur.demenskiy03@gmail.com>
2021-08-23 16:39:58 +0300

    DEV-30: fix button and styles

--- src/components/PageNotFound/PageNotFound.tsx
+++ src/components/PageNotFound/PageNotFound.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import { Empty, Typography, Anchor } from 'antd';
+import { Empty, Typography, Button } from 'antd';
 import 'antd/dist/antd.css';
 import './PageNotFound.scss';
 import { ArrowLeftOutlined } from '@ant-design/icons';
 
 const { Title, Paragraph, Text } = Typography;
-const { Link } = Anchor;
 
 const PageNotFound = () => (
   <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={false} className="page-not-found">
     <Title level={1}>404</Title>
     <Text type="secondary">Page Not Found</Text>
-    <br />
-    <br />
     <Paragraph>It looks like you found a glitch in the matrix...</Paragraph>
     <div className="link-to-home-page">
-      <ArrowLeftOutlined />
-      <Anchor>
-        <Link href="/" title="Back to Home Page" />
-      </Anchor>
+      <Button icon={<ArrowLeftOutlined />} type="link">
+        Back to home Page
+      </Button>
     </div>
   </Empty>
 );
