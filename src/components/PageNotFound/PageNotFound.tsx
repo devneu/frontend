0e754875a27def88b680a6b1da72fdb8e78277f2
Skitus <artur.demenskiy03@gmail.com>
2021-08-23 16:51:11 +0300

    DEV-30: delete style and fix commits

--- src/components/PageNotFound/PageNotFound.tsx
+++ src/components/PageNotFound/PageNotFound.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Empty, Typography, Button } from 'antd';
-import 'antd/dist/antd.css';
-import './PageNotFound.scss';
 import { ArrowLeftOutlined } from '@ant-design/icons';
 
 const { Title, Paragraph, Text } = Typography;
 
 const PageNotFound = () => (
-  <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={false} className="page-not-found">
+  <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={false}>
     <Title level={1}>404</Title>
     <Text type="secondary">Page Not Found</Text>
     <Paragraph>It looks like you found a glitch in the matrix...</Paragraph>
-    <div className="link-to-home-page">
-      <Button icon={<ArrowLeftOutlined />} type="link" href="/">
-        Back to home Page
-      </Button>
-    </div>
+    <Button type="link" href="/" icon={<ArrowLeftOutlined />}>
+      Back to home Page
+    </Button>
   </Empty>
 );
 
