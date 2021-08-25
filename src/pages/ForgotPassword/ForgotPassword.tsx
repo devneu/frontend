474298c4f93d72c9966786be390c890ebc52b36b
Anton Sydorovych <antsyd2@gmail.com>
2021-08-25 20:18:22 +0300

    fixed comments;)

--- src/pages/ForgotPassword/ForgotPassword.tsx
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -3,9 +3,8 @@ import './ForgotPassword.scss';
 import { Form, Input, Button, Row, Col, Typography } from 'antd';
 import { useForm } from 'react-hook-form';
 import FormErrorMessage from '../../components/FormErrorMessage/FormErrorMessage';
-import { EMAIL_PATTERN } from '../../utils/formValidations';
 
-const { Title, Link } = Typography;
+const { Title, Link, Text } = Typography;
 
 const ForgotPassword = () => {
   const {
@@ -15,41 +14,39 @@ const ForgotPassword = () => {
   } = useForm();
 
   // eslint-disable-next-line no-console
-  const onSubmit = (data: any) => console.log('SignIn basic', data);
-  const messageHeader: string = 'Forgot Your Password?';
-  const messageText: string = 'We get it, stuff hapens. Just enter your email address below and we\'ll send you a link to reset your password!';
+  const onSubmit = (data: any) => console.log('Reset password basic', data);
   return (
     <div className="forgot-password">
       <Row justify="center">
-        <Col className="img-block" xs={0} sm={0} md={10} lg={10} xl={9} xxl={8}>
+        <Col className="img-block" xs={0} sm={0} md={10} lg={8} xl={8} xxl={6}>
           <img src="https://i.imgflip.com/5jwx15.jpg" alt="Welcome on portal" />
         </Col>
 
-        <Col className="form-block" xs={20} sm={22} md={10} lg={10} xl={9} xxl={8}>
+        <Col className="form-block" xs={24} sm={24} md={10} lg={8} xl={8} xxl={6}>
           <Col span={18}>
-            <Form onFinish={handleSubmit(onSubmit)}>
-              <Title className="title-form" level={2}>
-                <h2>{messageHeader}</h2>
-                <p>{messageText}</p>
+            <div className="text-wrapper">
+              <Title className="title" level={3}>
+                <Text className="primary-text">Forgot Your Password?</Text>
+                <br />
               </Title>
-
+              <Text type="secondary" className="secondary-text">
+                We get it, stuff hapens. Just enter your email address below and we`ll send you a
+                link to reset your password!
+              </Text>
+            </div>
+            <Form onFinish={handleSubmit(onSubmit)}>
               <Form.Item>
-                <Input
-                  className="email"
-                  placeholder="Enter Email Address..."
-                  {...register('email', {
-                    required:
-                      'This input is required , maximum length is 50 symbols and has such template as: expample.exam@gmail.com',
-                    maxLength: 50,
-                    pattern: EMAIL_PATTERN,
-                  })}
+                <Input.Password
+                  className="password"
+                  placeholder="Password"
+                  {...register('password', { required: 'This input is required' })}
                 />
-                <FormErrorMessage message={errors.email?.message} />
+                <FormErrorMessage message={errors.password?.message} />
               </Form.Item>
 
               <Form.Item>
                 <Button
-                  className="_reset-password"
+                  className="reset-password"
                   block
                   type="primary"
                   htmlType="submit"
@@ -63,17 +60,13 @@ const ForgotPassword = () => {
 
               <div className="footer">
                 <br />
-                <Link href="/frontend/sign-out">
-                  <Button className="_create-account" block type="primary" shape="round">
-                    Create an Account
-                  </Button>
-                </Link>
+                <div className="link">
+                  <Link href="/sign-out">Create an Account</Link>
+                </div>
                 <br />
-                <Link href="/frontend/sign-in">
-                  <Button className="_login" block type="primary" shape="round">
-                    Already have an account? Login!
-                  </Button>
-                </Link>
+                <div className="link">
+                  <Link href="/sign-in">Already have an account? Login!</Link>
+                </div>
               </div>
             </Form>
           </Col>
--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -33,11 +33,11 @@ const SignIn = () => {
   return (
     <div className="sign-in">
       <Row justify="center">
-        <Col className="img-block" xs={0} sm={0} md={10} lg={10} xl={9} xxl={8}>
+        <Col className="img-block" xs={0} sm={0} md={0} lg={8} xl={6} xxl={6}>
           <img src="https://source.unsplash.com/K4mSJ7kc0As/600x800" alt="Welcome on portal" />
         </Col>
 
-        <Col className="form-block" xs={20} sm={22} md={10} lg={10} xl={9} xxl={8}>
+        <Col className="form-block" xs={20} sm={22} md={12} lg={8} xl={6} xxl={6}>
           <Col span={18}>
             <Form onFinish={handleSubmit(onSubmit)}>
               <Title className="title-form" level={2}>
@@ -46,9 +46,8 @@ const SignIn = () => {
 
               <Form.Item>
                 <Input
-                  className="email"
+                  className="input"
                   placeholder="Email"
-                  style={{ height: '45px' }}
                   {...register('email', {
                     required:
                       'This input is required , maximum length is 50 symbols and has such template as: expample.exam@gmail.com',
@@ -61,7 +60,7 @@ const SignIn = () => {
 
               <Form.Item>
                 <Input.Password
-                  className="password"
+                  className="input"
                   placeholder="Password"
                   {...register('password', { required: 'This input is required' })}
                 />
@@ -80,7 +79,7 @@ const SignIn = () => {
               />
 
               <Form.Item>
-                <Button block type="primary" htmlType="submit" shape="round">
+                <Button block className="button" type="primary" htmlType="submit" shape="round">
                   Login
                 </Button>
               </Form.Item>
@@ -97,6 +96,7 @@ const SignIn = () => {
                   render={(renderProps) => (
                     <Button
                       onClick={renderProps.onClick}
+                      className="button"
                       block
                       danger
                       htmlType="submit"
@@ -121,18 +121,12 @@ const SignIn = () => {
               <hr className="line" />
 
               <div className="footer">
-                <br />
-                <Link href="/forgot-password">
-                  <Button className="_forgot-password-button" block type="primary" shape="round">
-                    Forgot password ?
-                  </Button>
-                </Link>
-                <br />
-                <Link href="/sign-out">
-                  <Button className="_sign-out-button" block type="primary" shape="round">
-                    Create an Account!
-                  </Button>
-                </Link>
+                <div className="link">
+                  <Link href="/forgot-password">Forgot password ?</Link>
+                </div>
+                <div className="link">
+                  <Link href="/sign-out">Create an Account!</Link>
+                </div>
               </div>
             </Form>
           </Col>
