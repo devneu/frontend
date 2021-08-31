7c8b644a43da2ddeac1679275326eb7c2fa2c4c3
yefimchuk <dmitriy.yefimchuk@gmail.com>
2021-08-31 22:07:17 +0300

    DEV-57: start work with my branch

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,11 +1,32 @@
-import { Button } from 'antd';
 import React from 'react';
 import './Chat.scss';
+import 'antd/dist/antd.css';
+import { MailOutlined, MessageOutlined, CalendarOutlined, SmileOutlined } from '@ant-design/icons';
+import { Switch } from 'antd';
 
 const Chat = () => (
   <section>
-    <h1>Chat page</h1>
-    <Button> Hello world </Button>
+    <div className="main">
+      <div className="menu">
+        <div className="MenuTop"> Menu Logo</div>
+        <div className="MenuIcons">
+          <MailOutlined />
+          <MessageOutlined />
+          <CalendarOutlined />
+          <SmileOutlined />
+        </div>
+        <div className="MenuBottom">
+          <Switch defaultChecked size="small" />
+          <br />
+          <Switch size="small" defaultChecked />
+          <div className="Dark mode"> Dark Mode</div>
+        </div>
+      </div>
+
+      <div className="messages" />
+      <div className="chat" />
+      <div className="group" />
+    </div>
   </section>
 );
 
