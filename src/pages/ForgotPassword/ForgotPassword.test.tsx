cd8f085635d0646678b452884505f2d2f9e38dfe
Anton Sydorovych <antsyd2@gmail.com>
2021-09-06 20:30:06 +0300

    fixed test

--- src/pages/ForgotPassword/ForgotPassword.test.tsx
+++ src/pages/ForgotPassword/ForgotPassword.test.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import ForgotPassword from './ForgotPassword';
 
 describe('ForgotPassword', () => {
   it('Should successfully render', () => {
-    const wrapper = renderer.create(<ForgotPassword />).toJSON();
+    const wrapper = renderer
+      .create(
+        <BrowserRouter>
+          <ForgotPassword />
+        </BrowserRouter>,
+      )
+      .toJSON();
     expect(wrapper).toMatchSnapshot();
   });
 });
