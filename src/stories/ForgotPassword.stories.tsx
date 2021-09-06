3c45952f4490db5c6bfda7cbfdef330a4b50cd74
AntonSydorovych <45861810+AntonSydorovych@users.noreply.github.com>
2021-09-06 14:28:18 +0300

    DEV-29: Forgot-Password-Page (#35)
    Created Forgot-Password-Page

---
+++ src/stories/ForgotPassword.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import ForgotPassword from '../pages/ForgotPassword/ForgotPassword';
+
+export default {
+  title: 'Pages/ForgotPassword',
+  component: ForgotPassword,
+} as ComponentMeta<typeof ForgotPassword>;
+
+const Template: ComponentStory<typeof ForgotPassword> = () => <ForgotPassword />;
+
+export const Primary = Template.bind({});
